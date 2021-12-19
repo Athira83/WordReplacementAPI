59a75b89256c3281e5c9e93b31432f579e9c88a7
Athira83 <96038487+Athira83@users.noreply.github.com>
2021-12-19 22:22:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,18 @@
 from chalice import Chalice
-
-app = Chalice(app_name='wordreplacementAPI')
-
-
-@app.route('/')
-def index():
-    return {'hello': 'world'}
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+app = Chalice(app_name='stringconverterapi')
+def process_string(text):
+    
+    ''' The  function takes as input a string and adds a copy right symbol
+    after the names of the companies as specified in keywords '''
+    import re
+    keywords=['deloitte','oracle','google','microsoft','amazon']                # List of words which has to be replaced
+    response_text=''                                                            # Creating an empty response text
+    text = text.replace('%20',' ')                                              # Replacing spaces
+    text=text.split(' ')                                                        # Splitting the string by spaces
+    for word in text:                                                           # Recreating the text with new words
+        if word.lower() in keywords:                                                
+            word = word + u"\u00a9"
+        else:
+            pass
+        response_text +=word+' '
+    return response_text
